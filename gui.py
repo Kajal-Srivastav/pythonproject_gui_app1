edeb4b546dab5fb2b003a91c215bdc4e290b0c23
kajal <kajalsrivastav1705@gmail.com>
2024-02-25 01:25:20 +0530

    Initial commit

--- gui.py
+++ gui.py
@@ -1,1 +1,10 @@
-print("hi")
+# import functions
+import PySimpleGUI as Sg
+
+label = Sg.Text("Hi enter the item to to do list")
+input_value = Sg.InputText(tooltip="enter a value")
+button_val = Sg.Button("ADD")
+
+window_data = Sg.Window('this is my to-do app', layout=[[label], [input_value, button_val]])
+window_data.read()
+window_data.close()
